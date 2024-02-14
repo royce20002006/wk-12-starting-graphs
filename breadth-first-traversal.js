b60d8aed0f2b61da55c9df3876e78d27df368a72
Royce Calvin <royce20002006@yahoo.com>
2024-02-13 23:22:24 -0600

    error

--- breadth-first-traversal.js
+++ breadth-first-traversal.js
@@ -19,3 +19,5 @@ console.log("Second Test:")
 printBreadthFirst(6); // Prints 1 through 6 in Breadth-first order, starting with 6
                       // One possible solution:  6, 4, 3, 5, 2, 1
 console.log("Third Test:")
+printBreadthFirst(4); // Prints 1 through 6 in Breadth-first order, starting with 4
+                      // One possible solution:  4, 3, 5, 6, 2, 1
